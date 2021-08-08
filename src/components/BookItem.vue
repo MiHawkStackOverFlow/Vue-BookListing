ba4dfdc50474931c1b7a2962c4f90dd4b0556123
Abhishek Sharma <abhivsharma8@gmail.com>
2021-08-08 14:46:08 +0530

    Book Item component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <li v-for="book in books" :key="book.title">
+      {{ book.title }} : {{ book.author }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
