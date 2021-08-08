adc1fbeedbad140670bf9a0d0c7131d1c253a4ef
Abhishek Sharma <abhivsharma8@gmail.com>
2021-08-08 14:36:43 +0530

    show list of books using v-for

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <h1> {{ title }} </h1>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books" :key="book.title">
+        {{ book.title }} : {{ book.author }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BookList',
+  name: "BookList",
   data() {
     return {
-      title: 'All Books',
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+      ],
     };
   },
 };
@@ -17,7 +27,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
